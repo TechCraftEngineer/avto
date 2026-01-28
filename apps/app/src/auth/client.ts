1353bfbd5cbe56e1335210a232ef596a6b755132
Maksim <kodermax@gmail.com>
2026-01-28 11:29:10 +0300

    up

--- apps/app/src/auth/client.ts
+++ apps/app/src/auth/client.ts
@@ -1,10 +1,7 @@
 import { emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { env } from "../env";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_APP_URL,
-  basePath: "/api/auth",
   plugins: [emailOTPClient()],
 });
 
