3b8bc58ccea28bf097848f3f7ed419ed7e957f63
Maksim <kodermax@gmail.com>
2026-01-30 23:06:16 +0300

    up

--- apps/app/src/components/response-detail/tabs/proposal-tab.tsx
+++ apps/app/src/components/response-detail/tabs/proposal-tab.tsx
@@ -2,7 +2,7 @@
 
 import type { RouterOutputs } from "@qbs-autonaim/api";
 import { Separator } from "@qbs-autonaim/ui";
-import { formatCurrency } from "../constants";
+import { formatCurrency } from "../utils/constants";
 
 type GigResponseDetail = NonNullable<RouterOutputs["gig"]["responses"]["get"]>;
 type VacancyResponseDetail = NonNullable<
