504c1d40595f3e84f3fc56fff31a461afeda4a1e
Maksim <kodermax@gmail.com>
2026-02-01 15:34:39 +0300

    up

---
+++ packages/jobs-parsers/tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  inlineOnly: false,
+  sourcemap: false,
+  clean: true,
+  minify: isProduction,
+  tsconfig: "tsconfig.json",
+});
