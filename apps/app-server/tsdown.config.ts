ab71b374871df00ee15c842bd050dfbd150630c3
Maksim <kodermax@gmail.com>
2026-02-23 16:40:06 +0300

    fix

--- apps/app-server/tsdown.config.ts
+++ apps/app-server/tsdown.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "./src/index.ts",
-  format: "esm",
-  outDir: "./dist",
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  sourcemap: false,
   clean: true,
+  minify: true,
+  target: "es2022",
+  outDir: "./dist",
+  treeshake: true,
+  inlineOnly: false,
   noExternal: [/@qbs-autonaim\/.*/],
 });
